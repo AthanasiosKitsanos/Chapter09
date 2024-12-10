9ff515b420aad250450e4d1e950979efbda9deea
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2024-12-10 20:52:44 +0200

    Using the Serialization libraries like Newtonsoft.Json and System.Text.Json.

---
+++ WorkingWithSerialization/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml.Serialization; //  To use [XmlAttribute]
+
+namespace Packt.Shared;
+
+public class Person
+{
+    public Person() { } // To serialize a class, we need to create a parameterless Constructor, else an Unhandled Exception messege will appear.
+    public Person(decimal initialSalary)
+    {
+        Salary = initialSalary;
+    }
+
+    // By using [XmlAttribute], we modify the way the Class Attributes appear inside the Xml. e.i. FirstName will appear as fname inside the Xml.
+    [XmlAttribute("fname")]
+    public string? FirstName { get; set; }
+    [XmlAttribute("lname")]
+    public string? LastName { get; set; }
+    [XmlAttribute("DoD")]
+    public DateTime DateOfBirth { get; set; }
+    public HashSet<Person>? Children { get; set; }
+    protected decimal Salary { get; set; }
+}
+
---
+++ WorkingWithSerialization/Program.Helpers.cs
@@ -0,0 +1,25 @@
+﻿
+namespace WorkingWithSerialization;
+
+partial class MainProgram
+{
+    private static void SectionTitle(string title)
+    {
+        ConsoleColor previousColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine($"***{title}***");
+        Console.ForegroundColor = previousColor;
+    }
+
+    private static void OutputFileInfo(string path)
+    {
+        Console.WriteLine("***File Info***");
+        Console.WriteLine($"File: {GetFileName(path)}");
+        Console.WriteLine($"Path: {GetDirectoryName(path)}");
+        Console.WriteLine($"Size: {new FileInfo(path).Length:N0} bytes.");
+        Console.WriteLine("/------------------");
+        Console.WriteLine(File.ReadAllText(path));
+        Console.WriteLine("------------------/");
+    }
+}
+
